5e7ae82f1176b5fd9a64617879ab5fe892fff299
IvanValentini <34308333+IvanValentini@users.noreply.github.com>
2023-03-09 16:48:35 +0100

    Parsing

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello, world!")
+with open("00-example.txt") as f:
+    lines = f.read().splitlines()
+
+C, R, S = map(int,lines[0].split())
+sneake_len = list(map(int,lines[1].split()))
+
+matrix = [[0 for _ in range(C)] for _ in range(R)]
+
+for i in range(R):
+    matrix[i] = list(lines[i+2].split())
+
+print(matrix)
+
+
+output = ""
+with open("output.txt","w") as f:
+    f.write(output)
